0e444509499e4dc80832ec5de8df6f8576b8ea6c
ivanmang <ivanhi@live.hk>
2018-07-26 16:34:24 +0800

    Completed reverse string

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,17 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  var newstr ="";
+  var i = 0;
+  for(i = str.length ; i >= 0 ; i--){
+    newstr = newstr+ str.charAt(i);
+  }
+  return newstr;
+}
+
+// function reverse(str) {
+//   return str.split('').reduce((rev, char) => char + rev, '');
+// }
 
 module.exports = reverse;
